1152634ca4fe30801f33b0bd045d9634c0d52fba
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2022-07-28 13:58:47 -0300

    Added aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import * as path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +14,10 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		alias: {
+			'@lib': path.resolve('./src/lib'),
+			'@components': path.resolve('./src/lib/components'),
+		},
 	},
 };
 
